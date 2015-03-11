dacb2354d3b29ed0e39cc97450dfbad7e47faf0d
Scott Kulas <scott_kulas@yahoo.com>
2015-03-11 19:58:32 +0000

    added show and gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'users#new'
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
